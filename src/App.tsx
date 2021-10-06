8ed9ae889ed9e87083b6123629f222b40116dd37
johelder <johelder.arj@gmail.com>
2021-10-05 21:15:13 -0300

    configure loading async storage data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-
+import {Routes} from './routes';
 
 import AppLoading from 'expo-app-loading';
 
-import {AuthProvider} from './hooks/auth';
-import {SignIn} from './pages/SignIn';
+import {AuthProvider, useAuth} from './hooks/auth';
 
-
-import { ThemeProvider } from 'styled-components';
+import {ThemeProvider} from 'styled-components';
 import theme from './global/styles/theme';
 
 import {
@@ -27,8 +26,9 @@ export default function App() {
     Poppins_500Medium,
     Poppins_700Bold
   })
+  const {userStorageLoading} = useAuth();
 
-  if(!fontsLoaded) {
+  if(!fontsLoaded || userStorageLoading) {
     return <AppLoading />
   }
 
@@ -36,11 +36,9 @@ export default function App() {
     <>
       <StatusBar translucent backgroundColor="transparent" />
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
           <AuthProvider>
-            <SignIn />
+            <Routes />
           </AuthProvider>
-        </NavigationContainer>
       </ThemeProvider>
     </>
   );
